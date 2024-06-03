602c71e177ea41e2555c1fc9049fc7c7328b6ae7
liu2015 <176784830@qq.com>
2024-06-03 15:54:02 +0800

    递交

--- service/jztinfo/jztinfo.go
+++ service/jztinfo/jztinfo.go
@@ -3,6 +3,7 @@ package jztinfo
 import (
 	"demo25/global"
 	"demo25/model/jztinfo"
+	"demo25/model/jztinfo/request"
 )
 
 type JztinfoService struct {
@@ -11,4 +12,46 @@ type JztinfoService struct {
 func (s *JztinfoService) Create(info jztinfo.Jztinfo) (err error) {
 	err = global.DB.Create(&info).Error
 	return err
+}
+
+func (s *JztinfoService) Delete(id string) (err error) {
+	err = global.DB.Where("id =? ", id).Delete(&jztinfo.Jztinfo{}).Error
+	return err
+}
+
+func (s *JztinfoService) Deletes(ids []string) (err error) {
+	err = global.DB.Where("id in ?", ids).Delete(&[]jztinfo.Jztinfo{}).Error
+	return err
+}
+func (s *JztinfoService) Updates(info jztinfo.Jztinfo) (err error) {
+	err = global.DB.Where("id =?", info.ID).Updates(&info).Error
+	return err
+}
+
+func (s *JztinfoService) FindOne(id string) (list jztinfo.Jztinfo, err error) {
+	err = global.DB.Where("id =?", id).First(&list).Error
+	return list, err
+}
+
+func (s *JztinfoService) FindList(info request.Findjztinfo) (list []jztinfo.Jztinfo, total int64, err error) {
+	limts := info.PageSize
+	offset := info.PageSize * (info.Page - 1)
+	db := global.DB.Model(jztinfo.Jztinfo{})
+	var infos []jztinfo.Jztinfo
+
+	if info.StartCreatedAt != nil && info.EndCreatedAt != nil {
+		db = db.Where("created_at BETWEEN ? AND ?", info.StartCreatedAt, info.EndCreatedAt)
+	}
+	if info.Name != "" {
+		db = db.Where("name like ?", "%"+info.Name+"%")
+	}
+	err = db.Count(&total).Error
+	if err != nil {
+		return
+	}
+	if limts != 0 {
+		db = db.Limit(limts).Offset(offset)
+	}
+	err = db.Find(&infos).Error
+	return infos, total, err
 }
